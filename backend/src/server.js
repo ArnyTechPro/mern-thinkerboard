5e9cd7dc70bbe03f95a0d629fb34b895393adf20
ArnyTechPro <arny.techpro@gmail.com>
2025-10-25 13:19:36 +0200

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,23 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 
 import notesRoutes from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
 import rateLimiter from './middleware/rateLimiter.js';
 
-dotenv.config({ debug: 'false' });
+dotenv.config({ debug: 'false', override: 'true' });
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // Middleware
-app.use(cors({ origin: 'http://localhost:5173' }));  // Allow requests from frontend dev server
+if (process.env.NODE_ENV !== 'production') {
+    app.use(cors({ origin: 'http://localhost:5173' }));  // Allow requests from frontend dev server
+}
+
 app.use(express.json());    // This middleware will parse the json bodies: req.body
 app.use(rateLimiter);
 
@@ -24,6 +29,15 @@ app.use((req, res, next) => {
 
 app.use('/api/notes', notesRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+    });
+}
+
+// Start server after DB connection is established
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log('Server started on port', PORT);
@@ -32,7 +46,3 @@ connectDB().then(() => {
 
 // download mangodb local
 // understand whole code
-
-
-// frontend
-// npm run dev
